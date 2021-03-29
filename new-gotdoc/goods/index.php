1d96ccadbf9f156f13c1ce79372abb74982d18f8
pmaks <pmaks1985@gmail.com>
2021-03-29 14:31:22 +0300

    добавил карточку товара

---
+++ new-gotdoc/goods/index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/new-gotdoc/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/new-gotdoc/css/main.css">
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <div class="align-items-center banner">
+            </div>
+            <div class="d-flex justify-content-between">
+                <div>
+                    <nav class="navbar navbar-expand-lg p-0">
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item mr-4">
+                                    <a class="nav-link nav-link_menu pl-0" href="#">О компании</a>
+                                </li>
+                                <li class="nav-item mr-4">
+                                    <a class="nav-link nav-link_menu" href="#">Доставка и оплата</a>
+                                </li>
+                                <li class="nav-item mr-4">
+                                    <a class="nav-link nav-link_menu" href="#">Гарантии</a>
+                                </li>
+                                <li class="nav-item mr-4">
+                                    <a class="nav-link nav-link_menu" href="#">Статьи</a>
+                                </li>
+                                <li class="nav-item mr-4">
+                                    <a class="nav-link nav-link_menu" href="#">Акции</a>
+                                </li>
+                                <li class="nav-item mr-4">
+                                    <a class="nav-link nav-link_menu" href="#">Контакты</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div>
+                    <div class="d-flex mt-2 select-region">
+                        <div class="mr-3">
+                            <span class="select-region_text">Ваш регион:</span>
+                            <span class="select-region_city">
+                                <span class="dottedUnderline">Москва</span>⏷
+                            </span>
+                        </div>
+                        <div>
+                            <a href="#" class="btn-outline-danger select-region_button text-decoration-none">Войти</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between mt-3">
+                <div>
+                    <div class="site-name">Готовые документы</div>
+                    <div class="site-name_explanation">Автоматизированный помощник для подготовки к проектам</div>
+                </div>
+                <div class="text-right">
+                    <img class="mr-1" src="/new-gotdoc/img/phone.png" alt="phone"><a class="phone text-decoration-none" href="tel:+78005504908">8 (800) 550-49-08</a>
+                    <div class="opening-hours">пн-вс: 06:00 - 21:00</div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between mt-3">
+                <div class="d-flex">
+                    <div class="dropdown">
+                        <a class="catalog_btn d-flex align-items-center dropdown-toggle" href="#" id="navbarDropdownMenuLink" aria-haspopup="true" data-toggle="dropdown" aria-expanded="true">
+                            <img class="p-3" src="/new-gotdoc/img/catalog.png" alt="catalog">
+                            <div>Каталог документов</div>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li class="dropdown-submenu">
+                                <a class="dropdown-item dropdown-toggle d-flex align-items-center justify-content-between" href="#">
+                                    <div class="text-normal">Образовательные организации</div><i class="bi bi-chevron-right"></i>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Детский сад</a>
+                                    </li>
+                                    <ul class="pl-3">
+                                        <li class="dropdown-submenu dropdown-item_li">
+                                            <a class="dropdown-item dropdown-toggle d-flex align-items-center justify-content-between" href="#">Пожарная безопасность<i class="bi bi-chevron-right"></i></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Готовые документы по ПБ</a></li>
+                                                <li><a class="dropdown-item" href="#">Декларация ПБ</a></li>
+                                                <li><a class="dropdown-item" href="#">Формы документов</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown-item_li"><a class="dropdown-item" href="#">Первая помощь</a>
+                                        </li>
+                                    </ul>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Детский лагерь</a>
+                                    </li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Школа</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Техникум</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Коледж</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Лицей</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Университет</a>
+                                    </li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Институт</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Организация ДПО</a>
+                                    </li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Автошкола</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown-submenu"><a class="dropdown-item">
+                                    <div class="text-normal">Объекты размещения</div>
+                                </a></li>
+                            <li class="dropdown-submenu"><a class="dropdown-item">
+                                    <div class="text-normal">Производственные предприятия</div>
+                                </a>
+                            </li>
+                            <li class="dropdown-submenu"><a class="dropdown-item">
+                                    <div class="text-normal">Административные здания</div>
+                                </a></li>
+                            <li class="dropdown-submenu"><a class="dropdown-item">
+                                    <div class="text-normal">Торговые организации</div>
+                                </a></li>
+                            <li class="dropdown-submenu"><a class="dropdown-item">
+                                    <div class="text-normal">Офисные здания</div>
+                                </a></li>
+                            <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle d-flex align-items-center justify-content-between" href="#">
+                                    <div class="text-normal">Спортивные организации</div><i class="bi bi-chevron-right"></i>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Детский сад</a>
+                                    </li>
+                                    <ul class="pl-3">
+                                        <li class="dropdown-submenu dropdown-item_li">
+                                            <a class="dropdown-item dropdown-toggle d-flex align-items-center justify-content-between" href="#">Пожарная безопасность<i class="bi bi-chevron-right"></i></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a class="dropdown-item" href="#">Готовые документы по ПБ</a></li>
+                                                <li><a class="dropdown-item" href="#">Декларация ПБ</a></li>
+                                                <li><a class="dropdown-item" href="#">Формы документов</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown-item_li"><a class="dropdown-item" href="#">Первая помощь</a>
+                                        </li>
+                                    </ul>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Детский лагерь</a>
+                                    </li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Школа</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Техникум</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Коледж</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Лицей</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Университет</a>
+                                    </li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Институт</a></li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Организация ДПО</a>
+                                    </li>
+                                    <li><a class="dropdown-item font-weight-bold" href="#">Автошкола</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <nav class="navbar py-0">
+                            <form class="form-inline">
+                                <input class="form-control form-control_input" type="search" placeholder="Введите название документа" aria-label="Введите название документа">
+                                <button class="search_btn" type="submit"></button>
+                            </form>
+                        </nav>
+                    </div>
+                </div>
+                <div class="btn backet d-flex align-items-center">
+                    <img class="pr-3" src="/new-gotdoc/img/backet.png" alt="backet">
+                    <span class="backet_text">Корзина пуста</span>
+                </div>
+            </div>
+        </header>
+        <div class="content">
+            <div class="bread-crumbs my-3">
+                <a class="bread-crumps_link" href="#">Главная</a><i class="fa"></i>
+                <a class="bread-crumps_link" href="#">Каталог товаров</a>
+            </div>
+            <div class="row mx-0"></div>
+        </div>
+    </div>
+    <footer>
+        <div class="container">
+            <div class="footer d-flex justify-content-between">
+                <div><a class="footer-title text-decoration-none" href="/">Готовыедокументы. РФ</a>
+                </div>
+                <div class="footer-phone">
+                    <p><a class="text-decoration-none footer-phone_link" href="tel:88005509590">8 (800) 550-95-90</a>
+                    </p>
+                    <p>Звонок по России бесплатный</p>
+                    <p><a href="mailto:info@gotdoc.ru">info@gotdoc.ru</a></p>
+                </div>
+                <div class="footer-schedule">
+                    <p>График работы:</p>
+                    <p>пн-пт: 6:00-21:00</p>
+                    <p>(время московское)</p>
+                </div>
+                <div class="footer-nord-soft">
+                    <a class="text-decoration-none" href="https://nordsoftware.ru/">
+                        <p class="footer-nord-soft_title">Создание сайта:</p>
+                        <img src="/new-gotdoc/img/nord-soft.png" alt="nord-soft">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <script src="/new-gotdoc/js/jquery-3.5.1.min.js"></script>
+    <script src="/new-gotdoc/js/popper.min.js"></script>
+    <script src="/new-gotdoc/js/bootstrap.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+                if (!$(this).next().hasClass('show')) {
+                    $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+                }
+                var $subMenu = $(this).next('.dropdown-menu');
+                $subMenu.toggleClass('show');
+                $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+                    $('.dropdown-submenu .show').removeClass('show');
+                });
+                return false;
+            });
+
+            $('.dropdown').on("hidden.bs.dropdown", function() {
+                $('.dropdown-menu.show').removeClass('show');
+            });
+
+            $(document).on('click', '.dropdown-menu', function(e) {
+                $(this).hasClass('keep_open') && e
+                    .stopPropagation();
+            });
+
+        });
+    </script>
+
+</body>
+
+</html>
