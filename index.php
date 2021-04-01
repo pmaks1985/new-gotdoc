03872af05579fef1c07ed232f1bbf7f4e143f678
pmaks <pmaks1985@gmail.com>
2021-04-01 16:39:56 +0300

    переименовал и переподключил стили

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/new-gotdoc/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/new-gotdoc/css/main.css">
+    <link rel="stylesheet" href="/new-gotdoc/css/template_styles.css">
 </head>
 
 <body>
